b222883df9e9f0d21ee2de97d84cf196458e66b8
Noriel <N/A>
2022-07-11 11:46:33 +0200

    P5B Ex 5.7 Done

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [isFull, setIsFull] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if(!isFull) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button type="button" onClick={() => setIsFull(!isFull)}>Show</button>
+      </div>  
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      <ul>
+        <li>Title: {blog.title}</li>
+        <li>URL: <a href={blog.url}>{blog.url}</a></li>
+        <li>Likes: {blog.likes}</li>
+        <li>Author: {blog.author}</li>
+      </ul>
+      <button type="button" onClick={() => setIsFull(!isFull)}>Hide</button>
+    </div> 
+  )
+}
 
 export default Blog
